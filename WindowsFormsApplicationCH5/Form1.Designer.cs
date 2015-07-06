e6b78d87c55c00e00c64345bbc19185ef6398300
tkericlin <ntubme98@gmail.com>
2015-07-06 10:21:58 +0800

    專案初始化

---
+++ WindowsFormsApplicationCH5/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace WindowsFormsApplicationCH5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.T = new System.Windows.Forms.Label();
+            this.b0 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.bSign = new System.Windows.Forms.Button();
+            this.bDot = new System.Windows.Forms.Button();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.bSub = new System.Windows.Forms.Button();
+            this.bMul = new System.Windows.Forms.Button();
+            this.bDiv = new System.Windows.Forms.Button();
+            this.bBack = new System.Windows.Forms.Button();
+            this.bClear = new System.Windows.Forms.Button();
+            this.bEQ = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // T
+            // 
+            this.T.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.T.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.T.Location = new System.Drawing.Point(245, 50);
+            this.T.Name = "T";
+            this.T.Size = new System.Drawing.Size(411, 49);
+            this.T.TabIndex = 0;
+            this.T.Text = "0";
+            this.T.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(245, 358);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(64, 58);
+            this.b0.TabIndex = 1;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(245, 283);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(64, 58);
+            this.b1.TabIndex = 2;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(330, 283);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(64, 58);
+            this.b2.TabIndex = 3;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(418, 283);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(64, 58);
+            this.b3.TabIndex = 4;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(245, 208);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(64, 58);
+            this.b4.TabIndex = 5;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(330, 208);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(64, 58);
+            this.b5.TabIndex = 6;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(418, 208);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(64, 58);
+            this.b6.TabIndex = 7;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(245, 127);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(64, 58);
+            this.b7.TabIndex = 8;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(330, 127);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(64, 58);
+            this.b8.TabIndex = 9;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(418, 127);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(64, 58);
+            this.b9.TabIndex = 10;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            // 
+            // bSign
+            // 
+            this.bSign.Location = new System.Drawing.Point(330, 358);
+            this.bSign.Name = "bSign";
+            this.bSign.Size = new System.Drawing.Size(64, 58);
+            this.bSign.TabIndex = 11;
+            this.bSign.Text = "+/-";
+            this.bSign.UseVisualStyleBackColor = true;
+            this.bSign.Click += new System.EventHandler(this.bSign_Click);
+            // 
+            // bDot
+            // 
+            this.bDot.Location = new System.Drawing.Point(418, 358);
+            this.bDot.Name = "bDot";
+            this.bDot.Size = new System.Drawing.Size(64, 58);
+            this.bDot.TabIndex = 12;
+            this.bDot.Text = ".";
+            this.bDot.UseVisualStyleBackColor = true;
+            this.bDot.Click += new System.EventHandler(this.bDot_Click);
+            // 
+            // bAdd
+            // 
+            this.bAdd.Location = new System.Drawing.Point(506, 127);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(64, 58);
+            this.bAdd.TabIndex = 13;
+            this.bAdd.Tag = "+";
+            this.bAdd.Text = "+";
+            this.bAdd.UseVisualStyleBackColor = true;
+            this.bAdd.Click += new System.EventHandler(this.bAdd_Click);
+            // 
+            // bSub
+            // 
+            this.bSub.Location = new System.Drawing.Point(506, 208);
+            this.bSub.Name = "bSub";
+            this.bSub.Size = new System.Drawing.Size(64, 58);
+            this.bSub.TabIndex = 14;
+            this.bSub.Tag = "-";
+            this.bSub.Text = "-";
+            this.bSub.UseVisualStyleBackColor = true;
+            // 
+            // bMul
+            // 
+            this.bMul.Location = new System.Drawing.Point(506, 283);
+            this.bMul.Name = "bMul";
+            this.bMul.Size = new System.Drawing.Size(64, 58);
+            this.bMul.TabIndex = 15;
+            this.bMul.Tag = "*";
+            this.bMul.Text = "*";
+            this.bMul.UseVisualStyleBackColor = true;
+            // 
+            // bDiv
+            // 
+            this.bDiv.Location = new System.Drawing.Point(506, 358);
+            this.bDiv.Name = "bDiv";
+            this.bDiv.Size = new System.Drawing.Size(64, 58);
+            this.bDiv.TabIndex = 16;
+            this.bDiv.Tag = "/";
+            this.bDiv.Text = "/";
+            this.bDiv.UseVisualStyleBackColor = true;
+            // 
+            // bBack
+            // 
+            this.bBack.Location = new System.Drawing.Point(592, 127);
+            this.bBack.Name = "bBack";
+            this.bBack.Size = new System.Drawing.Size(64, 58);
+            this.bBack.TabIndex = 17;
+            this.bBack.Text = "Back";
+            this.bBack.UseVisualStyleBackColor = true;
+            this.bBack.Click += new System.EventHandler(this.bBack_Click);
+            // 
+            // bClear
+            // 
+            this.bClear.Location = new System.Drawing.Point(592, 208);
+            this.bClear.Name = "bClear";
+            this.bClear.Size = new System.Drawing.Size(64, 58);
+            this.bClear.TabIndex = 18;
+            this.bClear.Text = "C";
+            this.bClear.UseVisualStyleBackColor = true;
+            this.bClear.Click += new System.EventHandler(this.bClear_Click);
+            // 
+            // bEQ
+            // 
+            this.bEQ.Location = new System.Drawing.Point(592, 358);
+            this.bEQ.Name = "bEQ";
+            this.bEQ.Size = new System.Drawing.Size(64, 58);
+            this.bEQ.TabIndex = 19;
+            this.bEQ.Tag = "";
+            this.bEQ.Text = "=";
+            this.bEQ.UseVisualStyleBackColor = true;
+            this.bEQ.Click += new System.EventHandler(this.bEQ_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 542);
+            this.Controls.Add(this.bEQ);
+            this.Controls.Add(this.bClear);
+            this.Controls.Add(this.bBack);
+            this.Controls.Add(this.bDiv);
+            this.Controls.Add(this.bMul);
+            this.Controls.Add(this.bSub);
+            this.Controls.Add(this.bAdd);
+            this.Controls.Add(this.bDot);
+            this.Controls.Add(this.bSign);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.T);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label T;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button bSign;
+        private System.Windows.Forms.Button bDot;
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.Button bSub;
+        private System.Windows.Forms.Button bMul;
+        private System.Windows.Forms.Button bDiv;
+        private System.Windows.Forms.Button bBack;
+        private System.Windows.Forms.Button bClear;
+        private System.Windows.Forms.Button bEQ;
+    }
+}
+
---
+++ WindowsFormsApplicationCH5/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// 2015.06.26 增刪
+// 1.加入連續+-*/功能 
+// 2.按Clear後,問題會出現在+-*/之後
+// 3.[Line: 190-199] 試圖加入 Init功能 到 bClear_Click()中
+
+// 問題;
+// public 與 private差異? 
+// 用什麼語法或函式 將 private的值傳到外面的主程式中?
+// (Q1:什麼是NaN?)  (Q2: 放在這邊會變公用 )
+// (Q3: 跳出private void bClear_Click外面後, 紀錄值仍是之前輸入的 A值)
+// (Q4: 跳出private void bClear_Click外面後, 紀錄值仍是之前輸入的 A值)
+// (Q5: 跳出private void bClear_Click外面後, 紀錄值仍是之前輸入的 A值)
+
+
+
+namespace WindowsFormsApplicationCH5
+{
+    public partial class Form1 : Form
+    {
+        bool isStatusNew = true;
+
+        //int isClearNew =  
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.b1.Click += new System.EventHandler(this.b0_Click);
+            this.b2.Click += new System.EventHandler(this.b0_Click);
+            this.b3.Click += new System.EventHandler(this.b0_Click);
+            this.b4.Click += new System.EventHandler(this.b0_Click);
+            this.b5.Click += new System.EventHandler(this.b0_Click);
+            this.b6.Click += new System.EventHandler(this.b0_Click);
+            this.b7.Click += new System.EventHandler(this.b0_Click);
+            this.b8.Click += new System.EventHandler(this.b0_Click);
+            this.b9.Click += new System.EventHandler(this.b0_Click);
+            this.bSub.Click += new System.EventHandler(this.bAdd_Click);
+            this.bMul.Click += new System.EventHandler(this.bAdd_Click);
+            this.bDiv.Click += new System.EventHandler(this.bAdd_Click);
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            String S = e.KeyData.ToString();         //鍵盤名稱
+            String D = S.Substring(S.Length - 1);    //最後一個字元
+            String Q = S.Substring(0, S.Length - 1); //前面的其他字元
+            if ((Q == "D" && S.Length == 2) || Q == "NumPad")
+            {
+                if (T.Text == "0")
+                {
+                    T.Text = "";
+                }
+                T.Text += D;
+            }
+
+            //請到From_Load去設定其他 b1~b9 , 共用此副程式
+        }
+
+
+
+
+        private void b0_Click(object sender, EventArgs e)
+        {
+            if (isStatusNew == true)            // 設定isStatusNew的狀態,準備接新的數值
+            {
+                T.Text = "";                    // 把 T.Text = 0 的狀態消除變 "" 
+                isStatusNew = false;            // 把 isStatusNew設為false(不是全新空的數值),讓它之後走 T.Text += ((Button)sender).Text 來接新的數值
+            }
+
+            if (T.Text == "0")                  // 如果 T.Text 是 0
+            {
+                T.Text = "";                    // 把 T.Text = 0 的狀態消除變 ""
+            }
+
+            T.Text += ((Button)sender).Text;    // 讓先前 紀錄的數值 接上 新的數字到尾端
+        }
+
+
+
+        //輸入運算元
+        double A = Double.NaN;                    // 把 A 設為浮點數, 但先不給予值, 而是NaN                                                       (Q1:什麼是NaN?)  (Q2: 放在這邊會變公用 )
+        String op;                                // 設 參數op 去接未來的運算子
+        private void bAdd_Click(object sender, EventArgs e)
+        {
+            if (!Double.IsNaN(A))                 // 當 A = NaN 時 ==> Double.IsNaN(A) 值為 true ==> 所以 !true ==> false ==> 就不會進去
+            {
+                // Not First Click +              //因為不是第一次按+號,所以按完+號後,直接先做一次按"="的運算
+                bEQ_Click(null, null);            //bEQ_Click代表呼叫bEQ按下去的功能, 前後兩個數值不重要, 所以給null
+            }
+
+                // First Click +                  //因為從未按過+號, 所以要當A沒有值處理
+            A = double.Parse(T.Text);             //當第一次按+號的時候, 用 A 紀錄數字看板上的數值
+            //T.Text = "0";                       //不要把看板數字歸0
+            op = ((Button)sender).Tag.ToString(); //接著用op來紀錄目前點選之按鈕的Tag值(在bAdd下就是'+'了)
+            isStatusNew = true;                   //輸入運算元之後,後面因為要準備承接新的數字,要把isStatusNew改成true
+
+            //最後會到From_Load去設定其他 - * / 共用此副程式
+        }
+
+
+        //執行計算(=)符號
+        private void bEQ_Click(object sender, EventArgs e)
+        {
+                                                //按完Clear鍵之後, 這邊進入bEQ, 產生
+
+
+            double B = double.Parse(T.Text);    //用 B 來紀錄輸入的第二個數字
+            double C = 0;                       //宣告'變數C'準備承接計算的結果
+            switch (op)                         //根據先前op的輸入結果(輸入加減乘除號的內部Tag值)來決定作何種運算
+            {
+                case "+": C = A + B; break;
+                case "-": C = A - B; break;
+                case "*": C = A * B; break;
+                case "/": C = A / B; break;
+            }
+            T.Text = C.ToString();              //把 答案C 顯示在看板上
+            A = C;                              //把 答案C 設定給原來儲存在第一個計算數值A, 用來作連續運算使用
+            B = 0;                              //把 B歸0
+            C = 0;                              //把 C歸0
+        }
+        
+
+
+
+
+
+
+
+
+        //小數點
+        private void bDot_Click(object sender, EventArgs e)
+        {
+            if (T.Text.IndexOf(".") < 0)        //沒有找到"."就是-1, 會進到T.Text加入小數點; 有小數點的話, 就不執行
+            {
+                T.Text = T.Text + ".";
+            }
+        }
+
+        //改變數值'正負號'的功能
+        private void bSign_Click(object sender, EventArgs e)
+        {
+            if (T.Text != "0")
+            { 
+                if (T.Text.IndexOf("-") >=0)
+                {
+                    T.Text = T.Text.Replace("-","");  //把"-"取代成沒有負號
+                }
+                else
+                {
+                    T.Text = "-" + T.Text;
+                }
+
+            }
+        }
+
+
+
+
+        //清除功能鍵
+        private void bClear_Click(object sender, EventArgs e)
+        {
+            //T.Text = "0";             //把看板歸零
+            //A = 0;                    //把內部計算相關的參數也歸0
+            //op = "";                  //把內部計算相關的參數也歸0
+
+            
+            T.Text = "0";               //把看板歸零
+            isStatusNew = true;         //把isStatusNew 轉為新的(true)
+            double A = Double.NaN;      //把 A 指定設成 NaN                                                          (Q3: 跳出private void bClear_Click外面後, 紀錄值仍是之前輸入的 A值)
+            double B = 0;
+            double C = 0;
+            op = null;                  //把 op 改為 null
+            Init();                     // 新增; 呼叫 Init
+            //  InitializationEventAttribute                                                                         (Q4: 如何查詢使用方法?)
+        }
+
+        
+        //設定呼叫初始化
+        public void Init()
+        {
+            T.Text = "0";               //把看板歸零
+            isStatusNew = true;         //把isStatusNew 轉為新的(true)
+            double A = Double.NaN;      //把 A 指定設成 NaN                                                            (Q5: 跳出private void bClear_Click外面後, 紀錄值仍是之前輸入的 A值)
+            double B = 0;
+            double C = 0;
+            op = null;                  //把 op 改為 null
+        }
+
+        /*
+        public InitializationEventAttribute(string eventName)
+        {
+            
+        }
+        */
+
+        //倒退功能鍵
+        private void bBack_Click(object sender, EventArgs e)
+        {
+            string S = T.Text;      //用 S 來接數值看板的內容
+
+            if (S.Length > 1)       //判斷 S 的內容是否大於1
+            {
+                T.Text = (double.Parse(S.Substring(0, S.Length - 1))).ToString();   //如果大於1, 作正常刪除尾端字元的動作
+                                                                                    //Substring是部份字串的意思,第一個參數是起始字元的索引,第二個參數是取出的字元數目 [S.Length就是整個S字串的長度]
+            }
+            else 
+            {
+                T.Text = "0";                                                       //如果 S的長度只剩下一個字元, 那麼按下去不論內容為何都應該歸0
+                                                                                    //這樣如果看板值為0, 使用者若繼續按倒退按鈕時, 程式才部會當掉, 看板值會一直保持是0
+            }
+        }
+
+       
+
+
+        
+    }
+}
